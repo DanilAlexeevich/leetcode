5ef245e806ea04b45719509fa30e3534a09a785b
Danil Kholkin <danil.kholk1n@yandex.ru>
2026-04-03 18:36:25 +0500

    2621. Sleep

---
+++ src/Sleep.ts
@@ -0,0 +1,13 @@
+async function sleep(millis: number): Promise<void> {
+  return new Promise<void>(resolve => {
+    setTimeout(resolve, millis);
+  })
+}
+
+let t = Date.now()
+sleep(100).then(() => console.log(Date.now() - t)) // 100
+
+// Given a positive integer millis, write an asynchronous 
+// function that sleeps for millis milliseconds. It can 
+// resolve any value. Note that minor deviation from 
+// millis in the actual sleep duration is acceptable.
